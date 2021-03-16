01e8df159c54f8cc7d214d20ba72e5b305aab4fa
persi005 <vserednevdev@gmail.com>
2021-03-16 17:58:02 +0300

    Шаблон для "Завершённые игры"

--- src/GamePages/Game/GameListPage.js
+++ src/GamePages/Game/GameListPage.js
@@ -69,13 +69,23 @@ export default class GameListPage extends React.Component {
 									</div>
 
 									<div className="Cup">
-										<p className="Status">Счёт 8:6</p>
-										<div className="Info">
-											<p>{context.GetText('gamelist', 'labelCup')}</p>
-											<div className="IconCup"></div>
+										<div className="ELO">
+											<p>(1056)</p>
+										</div>
+
+										<div className="Score">
+											<p className="Status">Счёт 8:6</p>
+											<div className="Info">
+												<p>{context.GetText('gamelist', 'labelCup')}</p>
+												<div className="IconCup"></div>
+											</div>
+										</div>
+
+										<div className="ELO">
+											<p>(1086)</p>
 										</div>
 									</div>
-									
+
 									<div className="Player">
 										<div className="Logo"></div>
 										<p>ЧёКаво?</p>
@@ -88,9 +98,65 @@ export default class GameListPage extends React.Component {
 							<p className="GeneralSubtitle">{context.GetText('gamelist', 'sublabelFinished')}:</p>
 						
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li data-win="left">
+									<div className="Player">
+										<div className="Logo"></div>
+										<p>Clown02</p>
+									</div>
+
+									<p className="Status">10:5</p>
+									
+									<div className="Player">
+										<div className="Logo"></div>
+										<p>ANIM</p>
+									</div>
+								</li>
+
+								<li data-cup="1" data-win="left">
+									<div className="Player">
+										<div className="Logo"></div>
+										<p>ЧёКаво?</p>
+									</div>
+
+									<div className="Cup">
+										<div className="ELO">
+											<p>+2</p>
+											<p>(1198)</p>
+										</div>
+
+										<div className="Score">
+											<p className="Status">10:9</p>
+											<div className="Info">
+												<p>{context.GetText('gamelist', 'labelCup')}</p>
+												<div className="IconCup"></div>
+											</div>
+										</div>
+
+										<div className="ELO">
+											<p>-2</p>
+											<p>(1020)</p>
+										</div>
+									</div>
+
+									<div className="Player">
+										<div className="Logo"></div>
+										<p>Кирик</p>
+									</div>
+								</li>
+
+								<li data-win="right">
+									<div className="Player">
+										<div className="Logo"></div>
+										<p>EZWINB4</p>
+									</div>
+
+									<p className="Status">2:10</p>
+									
+									<div className="Player">
+										<div className="Logo"></div>
+										<p>Vinni</p>
+									</div>
+								</li>
 							</ul>
 						</div>
 					</div>
