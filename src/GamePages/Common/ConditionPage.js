19e20eb7ab03063bf99bec35878fb70da918f9ec
persi005 <vserednevdev@gmail.com>
2021-03-28 22:05:57 +0300

    Настроил правильное отображение окна "Соглашение" Добавил RU описание Настроил общий каркас

--- src/GamePages/Common/ConditionPage.js
+++ src/GamePages/Common/ConditionPage.js
@@ -1,52 +1,56 @@
-import React from 'react'
-import SignInLanguagePanel from '../../Components/SignInLanguagePanel'
-import {LanguageContext} from '../../Language/LangPack'
+import React from 'react';
+import SignInLanguagePanel from '../../Components/SignInLanguagePanel';
+import {LanguageContext} from '../../Language/LangPack';
+import HeadNavigation from '../../Components/HeadNavigation';
 
 
 export default class ConditionPage extends React.Component {
-    constructor(props, context) {
-      super(props);
-      this.state = {
-          currentLogin: 'Hello', 
-          currentToken: 'Token',
+	constructor(props, context) {
+	super(props);
+	this.state = {
+			currentLogin: 'Hello', 
+			currentToken: 'Token',
 
-          selectedItem: null,
-          checkedItem: null
-      };
+			selectedItem: null,
+			checkedItem: null
+	};
 
-      //this.changeItemState = this.changeItemState.bind(this);
+	//this.changeItemState = this.changeItemState.bind(this);
 
-    }
-  
-    render() { 
-       
-      
-          return (
-              <LanguageContext.Consumer>
-                {(context) =>
-                ( 
-                  
-                <div className='ConditionPage'>
+	}
 
-                  <SignInLanguagePanel lang='ru'></SignInLanguagePanel>
-                  
-                  <div className='ConditionTitle'>
-                    {context.GetText('condition', 'conditionTitle')}
-                  </div>
-                    
-                  <div className='ConditionBody'>
-                    <div>{context.GetText('condition', 'conditionBody')}</div>
-                  </div>
-                  <div  className="FooterArea">
-                    <button>{context.GetText('condition', 'continueButton')}</button>
-                  </div>
-                </div>
-                )}
-              </LanguageContext.Consumer>
-          );
-      
-    }
-  }
-  ConditionPage.contextType = LanguageContext;
-  
-  
+	render() { 
+		
+
+		return (
+			<LanguageContext.Consumer>
+				{(context) =>
+				(
+					<>
+						<HeadNavigation>
+							<button className="ButtonBack"></button>
+
+							<p className="HeadNavigationTitle">{context.GetText('condition', 'labelWindow')}</p>
+
+							<SignInLanguagePanel></SignInLanguagePanel>
+						</HeadNavigation>
+						
+						<div className="ConditionPage">
+							<p class="GeneralSubtitle">{context.GetText('condition', 'sublabelWindow')}</p>
+								
+							<div className="Content">
+								<p>{context.GetText('condition', 'license')}</p>
+							</div>
+						</div>
+
+						<div className="FooterArea">
+							<button>{context.GetText('condition', 'continueButton')}</button>
+						</div>
+					</>
+				)}
+			</LanguageContext.Consumer>
+		);
+	}
+}
+
+ConditionPage.contextType = LanguageContext;
--- src/GamePages/Game.js
+++ src/GamePages/Game.js
@@ -4,54 +4,52 @@ import PageHolder from './PageHolder.js'
 
 
 export default class Game extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          currentPage: 'Hello'
-          
-      };
-      this.handleChangePage = this.handleChangePage.bind(this);
+	constructor(props) {
+	super(props);
+	this.state = {
+			currentPage: 'Hello'
+			
+	};
+	this.handleChangePage = this.handleChangePage.bind(this);
 
-    }
-  
-    Pages = [   'Hello', 
-                'SignIn', 
-                'SignIn:Anonim', 
-                'SignIn:SignIn', 
-                'SignIn:SignIn:EnterCode',
-                'SignIn:SignIn:FailCode',
-                'SignIn:SignUp', 
-                'SignIn:SignUp:AlreadyExists', 
-                'SignIn:SignUp:Success', 
-                'Game:GameList', 
-                'Game:GameStart', 
-                'Game:FindRandom', 
-                'Game:FindBot', 
-                'Game:FindUser', 
-                'Settings:Settings', 
-                'Settings:Avatar', 
-                'ConditionPage'];
-    
-    handleChangePage(event) {
-        this.setState(state => ({
-            currentPage: event.target.value
-        }));
-      }
+	}
 
-    render() {
+	Pages = [   'Hello', 
+					'SignIn', 
+					'SignIn:Anonim', 
+					'SignIn:SignIn', 
+					'SignIn:SignIn:EnterCode',
+					'SignIn:SignIn:FailCode',
+					'SignIn:SignUp', 
+					'SignIn:SignUp:AlreadyExists', 
+					'SignIn:SignUp:Success', 
+					'Game:GameList', 
+					'Game:GameStart', 
+					'Game:FindRandom', 
+					'Game:FindBot', 
+					'Game:FindUser', 
+					'Settings:Settings', 
+					'Settings:Avatar', 
+					'ConditionPage'];
+	
+	handleChangePage(event) {
+		this.setState(state => ({
+			currentPage: event.target.value
+		}));
+	}
 
-        return ( 
-        <div className='Game'> 
-            <PageHolder currentPage={this.state.currentPage}></PageHolder>
-            <div className="AdHolder"> 
-                <select onChange={this.handleChangePage}>
-                    {this.Pages.map(x=> <option key={x} value={x}>{x}</option>)}
-                </select>
-            </div>
-        </div>
-        );
-      
-    }
-  }
-  
-  
+	render() {
+
+	return ( 
+		<div className="Game"> 
+				<PageHolder currentPage={this.state.currentPage}></PageHolder>
+				<div className="AdHolder"> 
+					<select onChange={this.handleChangePage}>
+						{this.Pages.map(x=> <option key={x} value={x}>{x}</option>)}
+					</select>
+				</div>
+		</div>
+	);
+	
+	}
+}
